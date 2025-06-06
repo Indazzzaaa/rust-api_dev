61cb6096e6e2ab439a33a867f18f77ce48482f17
Sumant Patel <patelsumant19@gmail.com>
2025-06-06 10:37:35 +0530

    Composed the routes

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,25 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/{name}", get(hanlder_hello2));
+    let routes_all = Router::new().merge(routes_hello());
 
     // region:    --- Start Server
     let addr = "127.0.0.1:8080";
     let listner = TcpListener::bind(addr).await.unwrap();
     println!("->> LISTENING on {addr}\n");
 
-    axum::serve(listner, routes_hello.into_make_service())
+    axum::serve(listner, routes_all.into_make_service())
         .await
         .unwrap();
     // endregion: --- Start Server
 }
 
-// region: --- Handler Hello
+// region: --- Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(hanlder_hello2))
+}
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
@@ -35,6 +38,7 @@ struct HelloParams {
 }
 
 // e.g., '/hello?name=Sumant'
+
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
@@ -49,4 +53,4 @@ async fn hanlder_hello2(Path(name): Path<String>) -> impl IntoResponse {
 
     Html(format!("Hello <strong> {name} </strong>"))
 }
-// endregion: --- Handler Hello
+// endregion: --- Routes Hello
