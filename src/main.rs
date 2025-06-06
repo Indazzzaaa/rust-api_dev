839f93b2c970507a3bfcb4ea6a7d96ac8caaa7c2
Sumant Patel <patelsumant19@gmail.com>
2025-06-06 08:01:53 +0530

    Added the path extractor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use axum::{
     Router,
-    extract::Query,
+    extract::{Path, Query},
     response::{Html, IntoResponse},
     routing::get,
     serve::Listener,
@@ -12,7 +12,9 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(hanlder_hello2));
 
     // region:    --- Start Server
     let addr = "127.0.0.1:8080";
@@ -32,10 +34,18 @@ struct HelloParams {
     name: Option<String>,
 }
 
+// e.g., '/hello?name=Sumant'
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong> {name} </strong>"))
+}
+
+// e.g., '/hello2/Sumant'
+async fn hanlder_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
 
     Html(format!("Hello <strong> {name} </strong>"))
 }
